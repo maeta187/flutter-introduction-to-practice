73540955112375129484fcc22fa2bca91b216d74
maeta187 <nexus18789@gmail.com>
2024-12-25 22:31:18 +0900

    🚚 : built_in_type のディレクトリ構造変更

---
+++ lib/chapter02/built_in_type/index.dart
@@ -0,0 +1,18 @@
+//　参照先のファイル内の変数は全てプライベート変数として扱う(_を先頭につける)
+import 'number.dart';
+import 'string.dart';
+import 'bool.dart';
+import 'list.dart';
+import 'set.dart';
+import 'map.dart';
+import 'record.dart';
+
+void main() {
+  printNumber();
+  printString();
+  printBool();
+  printList();
+  printSet();
+  printMap();
+  printRecord();
+}
