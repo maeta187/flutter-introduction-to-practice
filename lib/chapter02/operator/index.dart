c78fcbc05cdcd36a1dd06fa147659ed10a07ad0e
maeta187 <nexus18789@gmail.com>
2024-12-26 21:44:16 +0900

    ✨ : Dart/演算子まとめ

---
+++ lib/chapter02/operator/index.dart
@@ -0,0 +1,13 @@
+import 'arithmetic_operator.dart';
+import 'relational_operator.dart';
+import 'conditional_operator.dart';
+import 'cascade_notation.dart';
+import 'collection_operator.dart';
+
+void main() {
+  printArithmeticOperator();
+  printRelationalOperator();
+  printConditionalOperator();
+  printCascadeNotation();
+  printCollectionOperator();
+}
