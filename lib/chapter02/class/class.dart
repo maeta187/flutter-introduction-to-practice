aba7fb584494ed83b1f32c7500c443a80584a4c6
maeta187 <nexus18789@gmail.com>
2025-01-07 21:53:27 +0900

    ✨ : Dart/コンストラクトまとめ

--- lib/chapter02/class/class.dart
+++ lib/chapter02/class/class.dart
@@ -1,4 +1,7 @@
 export 'class.dart';
+import 'dart:convert'; // utf8で文字列をエンコード
+import 'package:crypto/crypto.dart'; // ハッシュ化用
+
 class _Point1 {
   // コンストラクタ
   /// `:`の後に初期化リストを記述する
@@ -67,4 +70,85 @@ void printClass() {
   print('Point2: (${point2.x}, ${point2.y})');
   print('Point3: (${point3.x}, ${point3.y})');
   print('User: ${user.id}, ${user.getPassword}');
+}
+
+// constantコンストラクタ
+class _ConstantClass {
+  /// クラスインスタンスをコンパイル時に定数化する
+  /// コンストラクタに`const`を付与する
+  /// インスタンス変数は全て再代入不可となる
+  const _ConstantClass(this.x, this.y);
+
+  final int x;
+  final int y;
+}
+
+void printConstantClass() {
+  /// constantコンストラクタは常にコンパイル時に定数を生成するとは限らない
+  /// constantコンストラクタの前に`const`を付与する、または`const`変数に代入した場合に、常に同じインスタンスを生成する
+  /// 無駄なインスタンス生成を避けることでパフォーマンスを向上させる
+  final constantClass1 = const _ConstantClass(10, 20);
+  const constantClass2 = _ConstantClass(10, 20);
+  final constantClass3 = _ConstantClass(10, 20);
+  print(constantClass1 == constantClass2); // true: 同じインスタンス
+  print(constantClass1 == constantClass3); // false: 異なるインスタンス
+}
+
+// 名前付きコンストラクタ
+class _NamedConstructor {
+  const _NamedConstructor(this.firstName, this.lastName);
+
+  /// 'クラス名.識別子'で名前付きコンストラクタを定義する
+  const _NamedConstructor.noName()
+      : firstName = 'John',
+        lastName = 'Doe';
+
+  /// 'this'自クラスの別のコンストラクタを呼び出す
+  const _NamedConstructor.anonymous() : this('foo', 'bar');
+
+  final String firstName;
+  final String lastName;
+}
+
+void printNamedConstructor() {
+  final namedConstructor1 = _NamedConstructor('Alice', 'Smith');
+  final namedConstructor2 = _NamedConstructor.noName();
+  final namedConstructor3 = _NamedConstructor.anonymous();
+  print(
+      'NamedConstructor1: ${namedConstructor1.firstName} ${namedConstructor1.lastName}');
+  print(
+      'NamedConstructor2: ${namedConstructor2.firstName} ${namedConstructor2.lastName}');
+  print(
+      'NamedConstructor3: ${namedConstructor3.firstName} ${namedConstructor3.lastName}');
+}
+
+// factoryコンストラクタ
+/// 必ずしも新しいインスタンスを生成する必要はない場合や初期化リストに記述できない場合に使用する
+/// 'factory'キーワードを使用してコンストラクトを実装する場合`return`を使用してインスタンスを返す必要がある
+class UserData {
+  static final Map<int, UserData> _cache = {};
+
+  UserData();
+
+  factory UserData.fromCache(int userId) {
+    print(_cache);
+    final cache = _cache[userId];
+    if (cache != null) {
+      // キャッシュがある場合はキャッシュを返す
+      return cache;
+    }
+
+    // キャッシュがない場合は新しいインスタンスを生成してキャッシュに追加
+    final newInstance = UserData();
+    _cache[userId] = newInstance;
+    return newInstance;
+  }
+}
+
+void printFactoryConstructor() {
+  final userData1 = UserData.fromCache(1);
+  final userData2 = UserData.fromCache(2);
+  final userData3 = UserData.fromCache(1);
+  print(userData1 == userData2); // false: 異なるインスタンス
+  print(userData1 == userData3); // true: 同じインスタンス
 }
