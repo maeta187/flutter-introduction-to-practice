9f5d2e1c6b70bea268b160a5f1b6747b9e11f804
maeta187 <nexus18789@gmail.com>
2025-01-12 13:42:41 +0900

    ✨ : Dart/クラスまとめ

---
+++ lib/chapter02/class/index.dart
@@ -0,0 +1,20 @@
+import 'class.dart';
+import 'extend.dart';
+import 'implicit_interface.dart';
+import 'extended_method.dart';
+import 'mixin.dart';
+import 'Enum.dart';
+import 'class_modifier.dart';
+
+void main() {
+  printClass();
+  printConstantClass();
+  printNamedConstructor();
+  printFactoryConstructor();
+  printExtend();
+  printImplicitInterface();
+  printExtendedMethod();
+  printMixin();
+  printEnum();
+  printClassModifier();
+}
