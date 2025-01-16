2a425457d3f064117264b62c33719889280fc36c
maeta187 <nexus18789@gmail.com>
2025-01-16 22:54:26 +0900

    ✨ Dart/Streamまとめ

--- lib/chapter02/asynchronous_process/stream.dart
+++ lib/chapter02/asynchronous_process/stream.dart
@@ -1,6 +1,7 @@
 export 'stream.dart';
 
 import 'dart:io';
+import 'dart:async';
 
 // Stream型
 /// 非同期的に連続した値を扱う
@@ -55,6 +56,7 @@ void _pauseSubscription() async {
 /// `yield`に続いて値を記述すると、その値が戻り値のStreamとして通知される
 /// 購読のキャンセルされた際は、次の`yield`文が実行されると関数の中断が行われる
 Stream<String> _streamReturnType() async* {
+  // throw 'Error';
   await Future.delayed(const Duration(seconds: 1));
   yield 'Dart';
   await Future.delayed(const Duration(seconds: 1));
@@ -77,11 +79,7 @@ Future<void> _nonCancelSubscription() async {
   }
 }
 
-Future<void> printStream() async {
-  _asynchronousStream();
-  _synchronousStream();
-  _cancelSubscription();
-  _pauseSubscription();
+Future<void> _executeStreamReturnType() async {
   _streamReturnType();
 
   /// Streamの終了時に処理を実行するにはlistenメソッドの`onDone`にコールバックを渡す
@@ -92,9 +90,123 @@ Future<void> printStream() async {
   });
 
   /// `async-await`と`for`を使用するとStream処理が終わるとfor文から抜ける
-  await for (final data in _streamReturnType()) {
+  // await for (final data in _streamReturnType()) {
+  //   print(data);
+  // }
+  // print('Done');
+
+  /// Streamの例外を処理するには`listen`メソッドの`onError`にコールバックを渡す
+  /// `listen`メソッドのだ第4引数に`cancelOnError`を`true`にすると、例外が発生した時に購読がキャンセルされる
+  _streamReturnType().listen((data) {
     print(data);
+  }).onError((error, stackTrace) {
+    print(error);
+    print(stackTrace);
+  });
+
+  /// `async-await`と`for`を使用する場合は`try-catch`を使用して例外をキャッチする
+  try {
+    await for (final data in _streamReturnType()) {
+      print(data);
+    }
+  } catch (e) {
+    print(e);
   }
-  print('Done');
-  // _infiniteLoop();
+}
+
+// StreamController
+/// StreamControllerは簡単にStreamを生成するためのクラス
+/// `_Counter`クラスは内部に`StreamController`を持ち、`increment`メソッドが呼ばれると`StreamController`に値を送信する
+/// `StreamController`への値の送信は`add`メソッドを使用する
+/// `StreamController`では外部からイベント送信ができるため、より柔軟なStreamを扱うことができる
+/// 例外を送信する`addError`メソッドや、購読されているかを判定する`hasListener`メソッドも提供されている
+/// `StreamController`は購読されなくても`add`メソッドで値を送信することができ、その値はバッファリングされ購読された時に一斉に通知される
+/// そのため用途によってはメモリを消費する可能性があるので注意が必要
+class _Counter {
+  int _count = 0;
+  final StreamController<int> _controller = StreamController<int>();
+
+  Stream<int> get stream => _controller.stream;
+
+  void increment() {
+    _count++;
+    _controller.sink.add(_count + 1);
+  }
+}
+
+Stream<String> _broadcastReturnType() async* {
+  await Future.delayed(const Duration(milliseconds: 500));
+  yield 'Dart';
+  await Future.delayed(const Duration(milliseconds: 500));
+  yield 'Kotlin';
+  await Future.delayed(const Duration(milliseconds: 500));
+  yield 'Swift';
+  await Future.delayed(const Duration(milliseconds: 500));
+  yield 'TypeScript';
+}
+
+// ブロードキャスト
+/// 1つのStreamに対して複数回購読すると例外が発生する
+/// 複数の購読者へイベント通知するには`asBroadcastStream`を使用する
+/// ブロードキャストタイプのStreamは最初に購読されたタイミングでものStreamの購読を開始する
+/// そのため①で待機している間は`_broadcastReturnType`関数の本体は実行されません
+/// ②で購読を開始すると、`_broadcastReturnType`関数の本体が実行され、`yield`で値が通知される
+/// ③で2つ目の購読がスタートすると。そのタイミングからイベント通知され、それまでの値は通知されない
+Future<void> _broadcast() async {
+  final broadcastStream = _broadcastReturnType().asBroadcastStream();
+
+  // ①
+  await Future.delayed(const Duration(milliseconds: 1100));
+
+  // ②
+  broadcastStream.listen((data) {
+    print('listener 1: $data');
+  });
+
+  await Future.delayed(const Duration(milliseconds: 1100));
+
+  // ③
+  broadcastStream.listen((data) {
+    print('listener 2: $data');
+  });
+}
+
+Future<void> _streamController() async {
+  final counter = _Counter();
+  counter.increment();
+  counter.increment();
+  counter.stream.listen((count) {
+    print('count: $count');
+  });
+
+  counter.increment();
+}
+
+// Streamを変更する
+Stream<int> _numberStream() {
+  return Stream.fromIterable(List.generate(10, (index) => index));
+}
+
+/// Streamの値を変換する`map`
+/// Streamの値をフィルタリングする`where`
+/// Streamの値の最大数を制限する`take`
+void _executeNumberStream() {
+  _numberStream()
+      .where((i) => i % 2 == 0)
+      .map((i) => i * 2)
+      .take(3)
+      .listen((i) {
+    print(i);
+  });
+}
+
+Future<void> printStream() async {
+  _asynchronousStream();
+  _synchronousStream();
+  _cancelSubscription();
+  _pauseSubscription();
+  _executeStreamReturnType();
+  _streamController();
+  _broadcast();
+  _executeNumberStream();
 }
